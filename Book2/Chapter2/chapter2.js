99b2c42e30abd3cb33d9107307e8100e149cc63e
Malcom-Yingwani <nsovomalcom@gmail.com>
2025-09-26 09:18:00 +0200

    Add files via upload

---
+++ Book2/Chapter2/chapter2.js
@@ -0,0 +1,15 @@
+// alert("Coding is interesting");
+// for (var i = 0; i < 300; i++)
+// {
+//     document.write ("Coding is \"so\" fun!");
+// }
+
+for (var i = 0; i < 300; i++) 
+    { 
+        document.write ("Joe said,\"Hi!\""); 
+    }
+
+for (var i = 0; i < 300; i++)
+    {
+        document.write ('Joe said,"Hi!"');    
+    }
